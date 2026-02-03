583dbfa071fb390f17542685b035715684b2b9e2
liz69 <josseling.gutierrez222402@udem.edu.ni>
2026-02-02 20:17:34 -0600

    Adjunto avance en el Program y carpetas asignadas

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -7,7 +7,36 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//new
+builder.Services.AddCors(op =>
+
+{
+    op.AddPolicy("AllowAll", policy =>
+
+    {
+        policy.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+
+    });
+
+
+});
+
+
+//new
 var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("/swagger/v1/swagger.json", "Api ReactaCore");
+    s.RoutePrefix = string.Empty;
+
+
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +44,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
