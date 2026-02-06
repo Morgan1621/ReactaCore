7fd5a3371eba9af15c55b0c8c2ef47540127d030
liz69 <josseling.gutierrez222402@udem.edu.ni>
2026-02-05 23:48:12 -0600

    Primer Interfaces creada

--- CAPA_APLICATION/Interfaces/IEstadosRepository.cs
+++ CAPA_APLICATION/Interfaces/IEstadosRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CAPA_DOMAIN;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,14 @@ namespace CAPA_APLICATION.Interfaces
 {
     public interface IEstadosRepository
     {
+
+        Task<IEnumerable<ESTADO>> ListarEstadoAsync();
+
+        Task<IEnumerable<ESTADO>> ListarEstadoPorNombreAsync();
+        Task NuevoEstadoAsync(ESTADO sTADO);
+        Task EditarEstadoAsync(ESTADO sTADO);
+
+        Task EliminarEstadoAsync(int Id);
 
 
 
