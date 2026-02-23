b87263a4e929ddfdd618402cc703d781de774802
Morgan1621 <josseling.gutierrez222402@udem.edu.ni>
2026-02-22 23:59:25 -0600

    Adjunto Mejoras en la Interfaces 2222026

---
+++ CAPA_APLICATION/Interfaces/IEventosRepository.cs
@@ -0,0 +1,22 @@
+﻿using CAPA_DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_APLICATION.Interfaces
+{
+    public interface IEventosRepository
+    {
+
+        Task<IEnumerable<Eventos>> ListarEventosAsync();
+
+        Task<IEnumerable<Eventos>> ListarEventosPorNombreAsync();
+        Task NuevoEventosAsync(Eventos eventos);
+        Task EditarEventosAsync(Eventos eventos);
+
+        Task EliminarEventosAsync(int Id);
+
+    }
+}
