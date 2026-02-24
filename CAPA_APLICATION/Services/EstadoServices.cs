331dddf5e45a70247e2373bcf6f298c7bcf33a34
Liz <josseling.gutierrez222402@udem.edu.ni>
2026-02-23 22:12:36 -0600

    Liz m (#15)
    * Adjunto Clase trabajada en la Domain
    
    * Adjunto CP aplication
    
    * Primer Interfaces creada
    
    * commit
    
    * 2 commit
    
    * Hago modificacion en la capa aplication/Services
    
    * Actualizacion en la carpteta Services
    
    * xxxx

---
+++ CAPA_APLICATION/Services/EstadoServices.cs
@@ -0,0 +1,55 @@
+﻿using CAPA_APLICATION.DTOs;
+using CAPA_APLICATION.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_APLICATION.Services
+{
+    public class EstadoServices
+    {
+
+        private readonly IEstadosRepository _Repository;
+
+        public EstadoServices(IEstadosRepository repository)
+        {
+            _Repository = repository;
+        }
+
+        //Listar Estado //
+
+
+
+        public async Task<IEnumerable <EstadoDTOs>> ListarEstado()
+        {
+            var listar = await _Repository.ListarEstadoAsync();
+
+            return listar.Select(p => new EstadoDTOs
+            {
+                EstadoId = p.EstadoId,
+                NombreEstado = p.NombreEstado,
+
+
+            });
+        }
+
+        public async Task<IEnumerable<EstadoDTOs>> ListarNombre(string Buscar)
+        {
+            if (string.IsNullOrWhiteSpace(Buscar))
+                return Enumerable.Empty<EstadoDTOs>();
+
+            var lista = await _Repository.ListarEstadoPorNombreAsync();
+
+            return lista.Select(p => new EstadoDTOs
+            {
+                EstadoId = p.EstadoId,
+                NombreEstado = p.NombreEstado,
+
+
+            });
+
+        }
+    }
+}
